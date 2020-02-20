0c44d91d09f2034eb07dabe449e5136e28373648
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:36:52 +0530

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            float b = 6.6f;
+            double c = 6.6666;
+            decimal d = 1.234m;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt.ToShortDateString());
+            Console.WriteLine(dt.ToLongDateString());
+            Console.WriteLine("today date" + dt);
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(b.GetType());
+            Console.WriteLine(c.GetType());
+            Console.WriteLine(d.GetType());
+
+
+
+        }
+    }
+}
